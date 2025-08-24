c0086efe97a98e85ed5b5b01a7836449318279f7
Alexey Snytnikov <snytav@gmail.com>
2025-08-24 20:36:32 +0700

    1/2 increased to 50

--- num_and_sym.m
+++ num_and_sym.m
@@ -2,9 +2,9 @@
 tiledlayout(2,2)
 % numerical sygnal
 % from Python: linspace(-1000.,1000.,128)
-[n_t,n_f1] = numerical_signal(128,1/2,-1000,1000);
+[n_t,n_f1] = numerical_signal(128,50,-1000,1000);
 [mask_ft,freq_support] = fft_and_shift(n_f1,n_t);
-[t,f1] = symbolic_signal(1/2);
+[t,f1] = symbolic_signal(50);
 FT1 = fourier(f1);
 
 nexttile
